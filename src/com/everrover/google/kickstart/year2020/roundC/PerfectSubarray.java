8f97bed2c119e7ae9bdb78f9b25747056cb1adc5
zealot97 <abhishekdeyol.a.k.a.lionheart@gmail.com>
2021-01-06 14:10:20 +0530

    round c - KS - 2020 - Q3 - finished - v1

--- src/com/everrover/google/kickstart/year2020/roundC/PerfectSubarray.java
+++ src/com/everrover/google/kickstart/year2020/roundC/PerfectSubarray.java
@@ -1,37 +1,28 @@
 package com.everrover.google.kickstart.year2020.roundC;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class PerfectSubarray {
-    private static int prefix[], perfectSq[], offsets[];
-    private static int solution(int []arr){
-        Map<Integer, Integer> map = new HashMap<>();
-        int res[] = new int[arr.length];
-
-        for(int j=0; j<prefix.length; j++){
-            int index = prefix[j]-offsets[j];
-            for(int i=0; i<perfectSq.length; i++){
-                int sqIndex = index-perfectSq[i];
-                if( sqIndex < 0 ) break;
-                if(map.containsKey(sqIndex)){
-                    res[j] += map.get(sqIndex);
-                }
+    private static long solution(int []arr, int left, int right){
+        long[] map = new long[right+left+1];
+        map[0]++;
+        int prefixSum = 0;
+        long res[] = new long[arr.length];
+        for(int j=0; j<arr.length; j++){
+            prefixSum += arr[j];
+            int index = prefixSum+left;
+            for(int i=0; (index-i*i)>=0; i++){
+                res[j] += map[index-i*i];
             }
-            if(!map.containsKey(index)){
-                map.put(index, 1);
-            } else {
-                map.put(index, map.get(index) + 1);
-            }
+            map[index]++;
         }
-
-        int ans = 0;
-        for(int r: res){
-            ans+=r;
+        long ans = 0;
+        for(long r: res){
+            ans += r;
         }
         return ans;
     }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -39,24 +30,16 @@ public class PerfectSubarray {
         int k = t;
         while(t-->0){
             final int n = scanner.nextInt();
-            final int arr[] = new int[n+1];
-            offsets = new int[n+1];
-            prefix = new int[n+1];
+            final int arr[] = new int[n];
+            int left = 0, right = 0;
 
             int max_A = Integer.MIN_VALUE;
-            for(int i=1; i<=n; i++){
+            for(int i=0; i<n; i++){
                 arr[i] = scanner.nextInt();
-                prefix[i] = prefix[i-1]+arr[i];
-                max_A = Integer.max(max_A, prefix[i]);
-                offsets[i] = Integer.min(offsets[i-1], prefix[i]);
+                if(arr[i]>0) right += arr[i];
+                else left -= arr[i];
             }
-            perfectSq = new int[(int)Math.ceil(Math.sqrt(prefix[prefix.length-1]))+1];
-            for(int i=0; i<perfectSq.length; i++){
-                perfectSq[i] = i*i;
-            }
-//            solution(matrix, num, 0, 0);
-            System.out.println("Case #"+(k-t)+": "+solution(arr));
-//            System.out.println("Case #"+(k-t)+": "+ans);
+            System.out.println("Case #"+(k-t)+": "+solution(arr, left, right));
         }
     }
 }
